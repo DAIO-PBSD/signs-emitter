8821b8d03e236b7c131f786cc929bf9303b12f20
Dorian <dorian.r.mv@gmail.com>
2020-11-08 18:14:22 -0500

    creación proyecto

---
+++ src/test/kotlin/daio/signsemitter/SignsEmitterApplicationTests.kt
@@ -0,0 +1,13 @@
+package daio.signsemitter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SignsEmitterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
